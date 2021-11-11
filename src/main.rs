1e79ac2f35444c36423e1583dcd583e7a16e8066
ede1998 <online@erik-hennig.me>
2021-11-11 23:10:00 +0100

    feat: add schema for config file

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 // On Windows platform, don't show a console when opening the app.
 #![windows_subsystem = "windows"]
 
+use anyhow::{Context, Result};
 use druid::{AppLauncher, PlatformError, WindowDesc};
 
 mod config;
@@ -9,12 +10,32 @@ mod loader;
 mod modal_host;
 mod updater;
 
-fn main() -> Result<(), PlatformError> {
+fn main() -> Result<()> {
+    if write_schema()? {
+        return Ok(());
+    }
+
     let config_result = config::init();
     match config_result.and_then(|_| loader::load_data()) {
         Ok(data) => run(data),
         Err(e) => run_startup_failed(e),
     }
+    .map_err(Into::into)
+}
+
+fn write_schema() -> Result<bool> {
+    use itertools::Itertools;
+
+    std::env::args_os()
+        .tuple_windows()
+        .find_map(|(pred, succ)| (pred == "--generate-config-schema").then(|| succ))
+        .map_or(Ok(false), |schema_file| {
+            let schema = schemars::schema_for!(config::AppConfig);
+            let schema =
+                serde_json::to_string_pretty(&schema).context("Failed to generate schema.")?;
+            std::fs::write(schema_file, schema).context("Failed to save schema to file.")?;
+            Ok(true)
+        })
 }
 
 fn run(data: Vec<loader::Translation>) -> Result<(), PlatformError> {
